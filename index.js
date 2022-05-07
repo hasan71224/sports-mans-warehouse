819145cab1c5f0ec488e18a00937cfdaef2d20a5
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-05-08 04:18:50 +0600

    add delete server

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run() {
             res.send(result)
         })
 
-        // //DELETE
+        //DELETE
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
